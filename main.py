1fd20898d3a4212ad1a03523e8796cdc53d9c6cd
A.Reed <adrian.ezequiel.vergara@gmail.com>
2021-10-28 13:52:46 -0700

    pep8ify and prettify

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import requests as req
 from urllib.parse import urlparse, parse_qs
 import re
 
-def main():
-    scrap_udemy_coupons()
-    scrap_smart()
 
 def scrap_udemy_coupons():
-    result_course_list= list()
+    result_course_list = list()
     coupons_list = list()
-    udemy_coupons = req.get("https://udemycoupons.me/", headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}).text
-    udemycoupons_soup = BeautifulSoup(udemy_coupons, 'html.parser')
-    grid = udemycoupons_soup.find_all("a", {"rel": "bookmark"}, href=True)
+    site_url = "https://udemycoupons.me/"
+    header_data = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                   " AppleWebKit/537.36 (KHTML, like Gecko)"
+                   " Chrome/95.0.4638.54"
+                   " Safari/537.36")
+    header = {"User-Agent": header_data}
+
+    udemy_coupons = req.get(site_url, headers=header).text
+    udemy_coupons_soup = BeautifulSoup(udemy_coupons, 'html.parser')
+    grid = udemy_coupons_soup.find_all("a", {"rel": "bookmark"}, href=True)
     for item in grid:
         coupons_list.append(item["href"])
-    coupons_list=list( dict.fromkeys(coupons_list) )
+    coupons_list = list(dict.fromkeys(coupons_list))
     for course in coupons_list:
         try:
-            course_html = req.get(course, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}).text
+            course_html = req.get(course, headers=header).text
             course_soup = BeautifulSoup(course_html, 'html.parser')
-            links = course_soup.find_all("a",{"target":'_blank'} ,href=True,text=True)
+            links = course_soup.find_all("a", {"target": "_blank"}, href=True, text=True)
             for link in links:
                 if '[ENROLL THE COURSE]' in link.text:
-                    udemy_link=link["href"]
+                    udemy_link = link["href"]
                     if validate(udemy_link):
                         result_course_list.append(udemy_link)
         except Exception as e:
@@ -47,7 +51,8 @@ def scrap_smart():
         try:
             course_html = req.get(course).text
             course_soup = BeautifulSoup(course_html, 'html.parser')
-            url = course_soup.find("a", {"class": "fasc-button fasc-size-xlarge fasc-type-flat"}, href=True)["href"]
+            class_info = {"class": "fasc-button fasc-size-xlarge fasc-type-flat"}
+            url = course_soup.find("a", class_info, href=True)["href"]
             u = urlparse(url)
             query = parse_qs(u.query, keep_blank_values=True)
             coupon_code = query.get('couponCode')[0]
@@ -60,13 +65,16 @@ def scrap_smart():
             print(f"Error getting course: {e}")
             continue
     return result_course_list
+
+
 def validate(udemy_link):
     txt = req.get(udemy_link).text
-    course_id = re.search("course-id=\"([0-9]+)\"", txt)[1]
-    coupon_code=get_coupon_code(udemy_link)
+    id_rx = re.compile('course-id=\"([0-9]+)\"')
+    course_id = id_rx.search(txt)[1]
+    coupon_code = get_coupon_code(udemy_link)
     url_string = (f"https://www.udemy.com/api-2.0/course-landing-components/{course_id}"
                   f"/me/?couponCode={coupon_code}&components=redeem_coupon,discount_expiration"
-                 )
+                  )
     validation = req.get(url_string).json()
 
     if all(k in validation for k in ("discount_expiration", "redeem_coupon")):
@@ -79,10 +87,17 @@ def validate(udemy_link):
         else:
             return False
 
+
 def get_coupon_code(url):
     u = urlparse(url)
     query = parse_qs(u.query, keep_blank_values=True)
     return query.get('couponCode')[0]
+
+
+def main():
+    scrap_udemy_coupons()
+    scrap_smart()
+
 
 if __name__ == "__main__":
     main()
