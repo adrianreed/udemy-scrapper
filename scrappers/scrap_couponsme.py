624cfcf6b3fcd0f609b5449dbc4b6264ab4c1c74
mxm001 <moure.lm@gmail.com>
2021-10-29 17:14:38 -0300

    scrappers modularization and parallelization util chunk function

--- scrappers/scrap_couponsme.py
+++ scrappers/scrap_couponsme.py
@@ -3,14 +3,40 @@ Scrapper for CouponsMe
 """
 
 from udemy_validator.validator import validate
-from scrappers.util import get_soup
+from scrappers.util import get_soup, chunks
+import threading
 
 
 def scrap_couponsme(site_url):
     print(f'Searching {site_url}')
-    valid_courses = list()
-    scrapped_courses = list()
 
+    scrapped_courses = landing_page_scrap(site_url)
+    hilosLinks = list(
+        chunks(scrapped_courses, int((len(scrapped_courses)/10)+1)))
+    threads = []
+    identificador = 0
+    for x in range(0, 9):
+        threads.append(threading.Thread(
+            target=courses_scrapping, args=(hilosLinks[x],)))
+        identificador = identificador+1
+        pass
+    for x in threads:
+        x.start()
+        pass
+    for x in threads:
+        x.join()
+        pass
+
+    return 0
+
+
+def landing_page_scrap(site_url):
+    """
+    Get the list of courses in the landing page
+    :param site_url: (str)
+    :return: scrapped_courses (list)
+    """
+    scrapped_courses = list()
     try:
         soup = get_soup(site_url)
         grid = soup.find_all("a", {"rel": "bookmark"}, href=True)
@@ -22,11 +48,21 @@ def scrap_couponsme(site_url):
         scrapped_courses.append(item["href"])
 
     scrapped_courses = list(dict.fromkeys(scrapped_courses))
+    return scrapped_courses
 
+
+def courses_scrapping(scrapped_courses):
+    """
+    Get the list of validated courses
+    :param scrapped_courses: (list)
+    :return: valid_courses (list)
+    """
+    valid_courses = list()
     for course in scrapped_courses:
         try:
             soup = get_soup(course)
-            links = soup.find_all("a", {"target": "_blank"}, href=True, text=True)
+            links = soup.find_all(
+                "a", {"target": "_blank"}, href=True, text=True)
             for link in links:
                 if '[ENROLL THE COURSE]' in link.text:
                     udemy_link = link["href"]
@@ -35,5 +71,4 @@ def scrap_couponsme(site_url):
         except Exception as e:
             print(f"Error getting {course}: {e}")
             continue
-
-    return 0
+    return valid_courses
--- scrappers/scrap_smartybro.py
+++ scrappers/scrap_smartybro.py
@@ -4,14 +4,41 @@ Scrapper for SmartyBro
 
 from udemy_validator.util import get_coupon_code
 from udemy_validator.validator import validate
-from scrappers.util import get_soup
+from scrappers.util import get_soup, chunks
+import threading
 
 
 def scrap_smartybro(site_url):
     print(f'Searching {site_url}')
-    scrapped_courses = list()
-    valid_courses = list()
 
+    scrapped_courses = landing_page_scrap(site_url)
+
+    hilosLinks = list(
+        chunks(scrapped_courses, int((len(scrapped_courses)/10)+1)))
+    threads = []
+    identificador = 0
+    for x in range(0, 8):
+        threads.append(threading.Thread(
+            target=courses_scrapping, args=(hilosLinks[x],)))
+        identificador = identificador+1
+        pass
+    for x in threads:
+        x.start()
+        pass
+    for x in threads:
+        x.join()
+        pass
+
+    return 0
+
+
+def landing_page_scrap(site_url):
+    """
+    Get the list of courses in the landing page
+    :param site_url: (str)
+    :return: scrapped_courses (list)
+    """
+    scrapped_courses = list()
     try:
         soup = get_soup(site_url)
         grid = soup.find_all("div", {"class": "item"})
@@ -23,23 +50,33 @@ def scrap_smartybro(site_url):
         tags = item.find_all("span", {"class": "tag-post"})
         for t in tags:
             if "Programming" in t.text:
-                link = item.find("h2", {"class": "grid-tit"}).find("a", href=True)["href"]
+                link = item.find("h2", {"class": "grid-tit"}
+                                 ).find("a", href=True)["href"]
                 scrapped_courses.append(link)
 
+    return scrapped_courses
+
+
+def courses_scrapping(scrapped_courses):
+    """
+    Get the list of validated courses
+    :param scrapped_courses: (list)
+    :return: valid_courses (list)
+    """
+    valid_courses = list()
     for course in scrapped_courses:
         try:
             course_soup = get_soup(course)
-            class_info = {"class": "fasc-button fasc-size-xlarge fasc-type-flat"}
+            class_info = {
+                "class": "fasc-button fasc-size-xlarge fasc-type-flat"}
             course_url = course_soup.find("a", class_info, href=True)["href"]
             coupon_code = get_coupon_code(course_url)
             result = course_url.split('?')[0]+"?couponCode="+coupon_code
         except Exception as e:
             print(f"Error processing {course}: {e}")
             continue
-
         if validate(result):
             valid_courses.append(result)
         else:
             continue
-
-    return 0
+    return valid_courses
