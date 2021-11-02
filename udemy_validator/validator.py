3ff8a518198dc6c7c3fffaa3a47297cf71cacc67
mxm001 <moure.lm@gmail.com>
2021-11-01 23:37:54 -0300

    changed validator to use logging instead of msgs

--- udemy_validator/validator.py
+++ udemy_validator/validator.py
@@ -2,6 +2,7 @@
 Udemy coupon validator
 """
 from util.get_site import get_site
+import logging
 from util.ud_url_parse import get_course_id, get_coupon_code
 
 
@@ -13,27 +14,30 @@ def validate(link):
                     exp_date: (str) if True
                     message: (str) if False
     """
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    file_handler = logging.FileHandler("validator.log", mode="w")
+    file_handler.setFormatter(formatter)
+
+    log = logging.getLogger(__name__)
+    log.addHandler(file_handler)
+    
     course_id = get_course_id(link)
     coupon_code = get_coupon_code(link)
-
+    log.info(f"validation of: {link}")
     if all(x is not None for x in (course_id, coupon_code)):
         url_string = (f"https://www.udemy.com/api-2.0/course-landing-components/{course_id}"
                       f"/me/?couponCode={coupon_code}&components=redeem_coupon,discount_expiration"
                       )
     else:
-        return dict(
-            ok=False,
-            message=f'Invalid link: {link}. Failed to get coupon code or course id.'
-        )
+        log.error(f"Invalid link: {link}. Failed to get coupon code or course id.")
+        return False
 
     site = get_site(url_string, headers={})
-    if not site['ok']:
-        message = site["message"]
-        return dict(
-            ok=False,
-            message=message
-        )
-    json = site['site'].json()
+    if not site["ok"]:
+        log.error( site["message"])
+        return False
+
+    json = site["site"].json()
 
     if all(x in json for x in ("discount_expiration", "redeem_coupon")):
         # Bool, means coupon is enabled
@@ -44,27 +48,18 @@ def validate(link):
         exp_date = json["discount_expiration"]["data"]["discount_deadline_text"]
 
         if not enabled:
-            return dict(
-                ok=False,
-                message=f'Invalid link: {link}. Coupon not enabled.'
-            )
+            log.error(f"Invalid link: {link}. Coupon not enabled.")
+            return False
+            
         elif details is not None:
-            return dict(
-                ok=False,
-                message=f'Invalid link: {link}. {details}'
-            )
+            log.error(f"Invalid link: {link}. {details}")
+            return False
         elif not exp_date:
-            return dict(
-                ok=False,
-                message=f'Invalid link: {link}. Failed to get expiration date.'
-            )
+            log.error(f"Invalid link: {link}. Failed to get expiration date.")
+            return False
         else:
-            return dict(
-                ok=True,
-                exp_date=exp_date
-            )
+            log.info(f"{link} expires in {exp_date}")
+            return True, exp_date 
     else:
-        return dict(
-            ok=False,
-            message=f'{link} may be already free. Link missing discount_expiration and data redeem_coupon data.'
-        )
+        log.error(f"{link} may be already free. Link missing discount_expiration and data redeem_coupon data.")
+        return False
